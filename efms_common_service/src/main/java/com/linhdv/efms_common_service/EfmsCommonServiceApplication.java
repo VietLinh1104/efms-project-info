3c5bbcc5c7c3c2e1fecf51accbb970f02adeb985
OS.NGS-MINHDQ <os.ngs-minhdq@vietinbank.vn>
2026-04-18 15:56:41 +0700

    [config] init

---
+++ efms_common_service/src/main/java/com/linhdv/efms_common_service/EfmsCommonServiceApplication.java
@@ -0,0 +1,16 @@
+package com.linhdv.efms_common_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
+
+@SpringBootApplication
+public class EfmsCommonServiceApplication {
+
+	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
+		SpringApplication.run(EfmsCommonServiceApplication.class, args);
+	}
+
+}
---
+++ efms_common_service/src/test/java/com/linhdv/efms_common_service/EfmsCommonServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linhdv.efms_common_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfmsCommonServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
